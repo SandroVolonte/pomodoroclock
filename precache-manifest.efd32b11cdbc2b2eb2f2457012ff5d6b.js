f73a0a7c0b621c2bd37c49848f5d52fdd6a99973
SandroVolonte <61095194+SandroVolonte@users.noreply.github.com>
2020-07-28 10:49:13 +0200

    Updates

---
+++ precache-manifest.efd32b11cdbc2b2eb2f2457012ff5d6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d618886db7345dd485b776c6d2b5e5",
+    "url": "/pomodoroclock/index.html"
+  },
+  {
+    "revision": "cbad956e4f1ef41909ca",
+    "url": "/pomodoroclock/static/css/main.b90f5884.chunk.css"
+  },
+  {
+    "revision": "737b07c6e7b87b3d2719",
+    "url": "/pomodoroclock/static/js/2.3c387c42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoroclock/static/js/2.3c387c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbad956e4f1ef41909ca",
+    "url": "/pomodoroclock/static/js/main.c90fe035.chunk.js"
+  },
+  {
+    "revision": "1b2b09a1669aa4b605fe",
+    "url": "/pomodoroclock/static/js/runtime-main.f7f828c9.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/pomodoroclock/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "31393b87364e2a84df8b4f66fe874086",
+    "url": "/pomodoroclock/static/media/forest.31393b87.mp3"
+  }
+]);
